dbdbbd7cab196a91739bd5caa3a2ed6647f41208
KregSHooD <yangyundan88@163.com>
2021-04-09 15:16:32 +0800

    测试代码

--- src/lib/App/Debug/Controller/Index.php
+++ src/lib/App/Debug/Controller/Index.php
@@ -15,7 +15,16 @@ use CK\Database\DBA;
 
 class Index extends Controller {
     public function acIndex() {
-        $data = Model::inst('classify')->find(['id[!]'=>'1']);
+        $data = Model::inst('classify')->find([
+            'AMD' => [
+                'id[!]'=>'1',
+                'sort'=>'0'
+            ],
+            'OR'=>[
+                'id'=>'2',
+                'sort[>]'=>'0'
+            ]
+        ]);
         var_dump($data);
         echo DBA::inst()->sql_str;
     }
