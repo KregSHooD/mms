822c8a813b064b5d76c580d8b1936a982b429f40
KregSHooD <yangyundan88@163.com>
2021-05-11 17:38:28 +0800

    函数操作类添加，并加入CMS发布函数库目录

---
+++ src/lib/App/Cms/Controller/FuncManage.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * CK 开发框架
+ * User: Clake
+ * Date: 2017/2/15
+ * Time: 11:34
+ */
+
+namespace App\Cms\Controller;
+
+
+use App\Cms\Model\Template;
+use CK\Core\Controller;
+use CK\Util\Cipher;
+use CK\Util\IO\Directory;
+
+/**
+ * 模板方法操作类
+ * Class FuncManage
+ *
+ * @res true
+ * @package App\Cms\Controller
+ */
+class FuncManage extends Controller {
+
+    /**
+     * 查询模板方法集
+     * @param $tmp_id
+     * @param $query
+     * @param $num
+     * @param $page
+     *
+     * @res true
+     * @return bool|array
+     */
+    public function query($tmp_id,$query,$page,$num) {
+        $tmp_id = Cipher::inst()->decrypt($tmp_id);
+        if (empty($tmp_id)) {
+            return false;
+        }
+
+        $fields = [
+            'fun_id'=>'',
+            'tmp_id'=>'',
+            'fun_name'=>'',
+            'fun_class'=>'',
+            'fun_var'=>'',
+            'fun_params'=>'',
+            'created_date'=>''
+        ];
+
+        $where = ['tmp_id'=>$tmp_id];
+
+        foreach ($query as $result) {
+            if (!empty($result['value'])) {
+                $column = $result['name']."[{$result['type']}]";
+                $where[$column] = $result['value'];
+            }
+        }
+
+        $result = Template::inst()->query($fields,$where,null,null,$num,$page,function($row){
+            $row['cipher_id'] = Cipher::inst()->encrypt($row['fun_id']);
+            return $row;
+        });
+        return $result;
+    }
+
+    /**
+     * 获取所有发布库类列表
+     *
+     * @res true
+     * @return array
+     */
+    public function getPublishClassList() {
+        $path = APP_LIB_PATH.'App/Cms/Publish/';
+        $list = Directory::getDir($path);
+        return $list;
+    }
+}
