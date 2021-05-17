5e7ecd8309fbc7316996dcdf219981a9f4a4992c
KregSHooD <yangyundan88@163.com>
2021-05-17 20:32:37 +0800

    添加、修改文章分类

--- src/lib/App/Cms/Controller/ArticleCategoryManage.php
+++ src/lib/App/Cms/Controller/ArticleCategoryManage.php
@@ -13,6 +13,7 @@ use App\Cms\Model\ArticleCategory;
 use App\Cms\Model\Template;
 use CK\Core\Controller;
 use CK\Util\Cipher;
+use CK\Util\Pinyin;
 
 /**
  * 文章分类管理操作类
@@ -21,7 +22,8 @@ use CK\Util\Cipher;
  * @res true
  * @package App\Cms\Controller
  */
-class ArticleCategoryManage extends Controller {
+class ArticleCategoryManage extends Controller
+{
 
     /**
      * 查询文章分类
@@ -32,27 +34,28 @@ class ArticleCategoryManage extends Controller {
      * @res true
      * @return array
      */
-    public function query($query,$num,$page) {
+    public function query($query, $num, $page)
+    {
         $fields = [
-            'ctg_id'=>'',
-            'ctg_name'=>'',
-            'ctg_publish_path'=>'',
-            'tmp_id'=>'',
-            'tmp_name'=>'',
-            'created_date'=>'',
-            'modified_date'=>''
+            'ctg_id' => '',
+            'ctg_name' => '',
+            'ctg_publish_path' => '',
+            'tmp_id' => '',
+            'tmp_name' => '',
+            'created_date' => '',
+            'modified_date' => ''
         ];
 
         $where = [];
 
         foreach ($query as $result) {
             if (!empty($result['value'])) {
-                $column = $result['name']."[{$result['type']}]";
+                $column = $result['name'] . "[{$result['type']}]";
                 $where[$column] = $result['value'];
             }
         }
 
-        $result = ArticleCategory::inst()->query($fields,$where,null,null,$num,$page,function($row){
+        $result = ArticleCategory::inst()->query($fields, $where, null, null, $num, $page, function ($row) {
             $row['cipher_id'] = Cipher::inst()->encrypt($row['ctg_id']);
             return $row;
         });
@@ -66,13 +69,60 @@ class ArticleCategoryManage extends Controller {
      * @res true
      * @return array|bool
      */
-    public function info($cipher_id){
+    public function info($cipher_id)
+    {
         $ctg_id = Cipher::inst()->decrypt($cipher_id);
-        if(!empty($ctg_id)){
-            $category = ArticleCategory::inst()->find(['ctg_id'=>$ctg_id]);
+        if (!empty($ctg_id)) {
+            $category = ArticleCategory::inst()->find(['ctg_id' => $ctg_id]);
             return $category;
         }
         return false;
+    }
+
+    /**
+     * 获取子分类
+     * @param string $ctg_id 父类ID
+     * @param string $ctg_name 父类名称
+     *
+     * @res true
+     * @return array|bool
+     */
+    public function getCategoryChild($ctg_id = '', $ctg_name = '')
+    {
+        $fields = [
+            'ctg_id' => '',
+            'ctg_name' => '',
+            'ctg_parent_id' => '',
+            'ctg_parent_name' => ''
+        ];
+        if (!empty($ctg_id)) {
+            $where['ctg_parent_id'] = $ctg_id;
+        } else {
+            if (empty($ctg_name)) {
+                $where['ctg_parent_id'] = null;
+            } else {
+                $where['ctg_name'] = $ctg_name;
+                $category = ArticleCategory::inst()->find(['ctg_name' => $ctg_name]);
+                if ($category) {
+                    $where['ctg_parent_id'] = $category['ctg_id'];
+                } else {
+                    return false;
+                }
+            }
+        }
+        $category = ArticleCategory::inst()->query($fields, $where, null, null, 1000);
+        return $category;
+    }
+
+    /**
+     * 获取汉字拼音
+     * @param $string
+     *
+     * @res true
+     * @return string
+     */
+    public function getPinyin($string){
+        return Pinyin::inst()->getAll($string);
     }
 
     /**
@@ -82,19 +132,20 @@ class ArticleCategoryManage extends Controller {
      * @res true
      * @return bool
      */
-    public function save($data){
-        $template = Template::inst()->find(['tmp_id'=>$data['tmp_id']]);
-        if($template){
+    public function save($data)
+    {
+        $template = Template::inst()->find(['tmp_id' => $data['tmp_id']]);
+        if ($template) {
             $data['tmp_name'] = $template['tmp_name'];
         }
-        if(empty($data['ctg_id'])){
+        if (empty($data['ctg_id'])) {
             unset($data['ctg_id']);
             $data['created_date'] = CK_NOW;
             $rel = ArticleCategory::inst()->insert($data);
             return $rel;
-        }else{
+        } else {
             $data['modified_date'] = CK_NOW;
-            $rel = ArticleCategory::inst()->update($data,['ctg_id'=>$data['ctg_id']]);
+            $rel = ArticleCategory::inst()->update($data, ['ctg_id' => $data['ctg_id']]);
             return $rel;
         }
     }
@@ -106,10 +157,11 @@ class ArticleCategoryManage extends Controller {
      * @res true
      * @return bool
      */
-    public function delete($cipher_id){
+    public function delete($cipher_id)
+    {
         $ctg_id = Cipher::inst()->decrypt($cipher_id);
-        if(!empty($ctg_id)){
-            $rel = ArticleCategory::inst()->delete(['ctg_id'=>$ctg_id]);
+        if (!empty($ctg_id)) {
+            $rel = ArticleCategory::inst()->delete(['ctg_id' => $ctg_id]);
             return $rel;
         }
         return false;
