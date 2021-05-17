d2c2d3d35710f3088081f0cd835b802ea6ae3955
KregSHooD <yangyundan88@163.com>
2021-05-17 16:11:11 +0800

    添加、修改文章分类

--- src/lib/App/Cms/Controller/ArticleCategoryManage.php
+++ src/lib/App/Cms/Controller/ArticleCategoryManage.php
@@ -84,7 +84,7 @@ class ArticleCategoryManage extends Controller {
     public function save($data){
         if(empty($data['ctg_id'])){
             unset($data['ctg_id']);
-            $data['create_date'] = CK_NOW;
+            $data['created_date'] = CK_NOW;
             $rel = ArticleCategory::inst()->insert($data);
             return $rel;
         }else{
