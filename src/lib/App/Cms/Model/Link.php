30a7f57d0973fe3e9c150ac50f2b96a3aa2ccd19
KregSHooD <yangyundan88@163.com>
2021-05-18 18:49:53 +0800

    导航管理、导航分类管理

---
+++ src/lib/App/Cms/Model/Link.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 友情链接模型
+ * User: liberty.lai
+ * Date: 2017/2/22
+ * Time: 17:43
+ */
+class Link extends \CK\Core\Model
+{
+    public function __construct()
+    {
+        parent::__construct('t_link');
+    }
+}
---
+++ src/lib/App/Cms/Model/LinkCategory.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * 友情链接分类
+ * User: liberty.lai
+ * Date: 2017/2/22
+ * Time: 17:45
+ */
+class LinkCategory extends \CK\Core\Model
+{
+    public function __construct()
+    {
+        parent::__construct('t_link_category');
+    }
+}
