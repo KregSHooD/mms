319b14aa4863bf57b0525f8f8a125947593dcbf1
KregSHooD <yangyundan88@163.com>
2021-05-18 15:06:29 +0800

    update

--- src/lib/App/Cms/Controller/PublishManage.php
+++ src/lib/App/Cms/Controller/PublishManage.php
@@ -11,6 +11,29 @@ namespace App\Cms\Controller;
 
 use CK\Core\Controller;
 
+/**
+ * 发布控制类
+ * Class PublishManage
+ *
+ * @res true
+ * @package App\Cms\Controller
+ */
 class PublishManage extends Controller {
 
+
+    public function publishIndex() {
+
+    }
+
+    public function publishCategory() {
+
+    }
+
+    public function publishArticle() {
+
+    }
+
+    public function publishSpecial() {
+
+    }
 }
