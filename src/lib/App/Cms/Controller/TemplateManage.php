bb651962431a1850928a28ac28f8496bba36a7aa
KregSHooD <yangyundan88@163.com>
2021-05-18 11:20:11 +0800

    获取子分类，获取拼音功能

--- src/lib/App/Cms/Controller/TemplateManage.php
+++ src/lib/App/Cms/Controller/TemplateManage.php
@@ -48,7 +48,8 @@ class TemplateManage extends Controller {
                 $where[$column] = $result['value'];
             }
         }
-
+        print_r($query);
+        print_r($where);die;
         $result = Template::inst()->query($fields,$where,null,null,$num,$page,function($row){
             $row['cipher_id'] = Cipher::inst()->encrypt($row['tmp_id']);
             return $row;
